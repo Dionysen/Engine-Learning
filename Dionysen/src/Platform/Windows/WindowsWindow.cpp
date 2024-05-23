28d53cb6f46ce25e80afd84fec1047448b9d5743
Dionysen <solongnight@outlook.com>
2024-05-23 14:13:26 +0800

    Update WindowsWindow.cpp

--- Dionysen/src/Platform/Windows/WindowsWindow.cpp
+++ Dionysen/src/Platform/Windows/WindowsWindow.cpp
@@ -58,8 +58,8 @@ namespace Dionysen
         {
             int success = glfwInit();
             DION_CORE_ASSERT(success, "Could not initialize GLFW");
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
             glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
